ae755383c972b995db3047fbf52c9b965cc14967
marcoghezzi1 <marco.ghezzi3@studenti.unimi.it>
2021-04-09 18:14:47 +0200

    now teatro lab1 is correct

--- teatro/src/main/java/Reservations.java
+++ teatro/src/main/java/Reservations.java
@@ -1,9 +1,10 @@
 public class Reservations {
     int reserved_seat = 0;
     private int posti_totali = 2;
+    private final Object x = reserved_seat;
 
     public int check_seats() {
-        synchronized (this) {
+        synchronized (x) {
             if (posti_totali-reserved_seat > 0){
                 return posti_totali-reserved_seat;
             }
@@ -15,8 +16,8 @@ public class Reservations {
     }
 
     public void prenota_biglietto() throws InterruptedException {
-        synchronized (this) {
-            Thread.sleep(20000);
+        synchronized (x) {
+            Thread.sleep(10000);
             reserved_seat += 1;
         }
     }
